e833ecd674669cc58812734fd0041fa4b6210713
Kaden Inskeep <tsukadenskeep@gmail.com>
2023-06-01 11:09:54 -0700

    final changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,37 @@
-const diagnostics = require('express').Router();
+const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile } = require('../helpers/fsUtils');
 
-// GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
-  readFromFile('./db/diagnostics.json').then((data) =>
-    res.json(JSON.parse(data))
-  );
+// GET Route for retrieving notes
+router.get('/notes', (req, res) => {
+  readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)))
+    .catch((err) => res.status(500).json({ error: 'Failed to read the database file.' }));
 });
+
+// POST Route for creating a new note
+router.post('/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (!title || !text) {
+    return res.status(400).json({ error: 'Note title and text are required.' });
+  }
+
+  const newNote = {
+    id: uuidv4(),
+    title,
+    text,
+  };
+
+  readFromFile('./db/db.json')
+    .then((data) => {
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+      writeToFile('./db/db.json', notes)
+        .then(() => res.json(newNote))
+        .catch((err) => res.status(500).json({ error: 'Failed to save the note to the database.' }));
+    })
+    .catch((err) => res.status(500).json({ error: 'Failed to read the database file.' }));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
-// const { clog } = require('./middleware/clog');
-// const apiRoutes = require('./routes/apiRoutes.js');
+const apiRoutes = require('./routes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-// app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
