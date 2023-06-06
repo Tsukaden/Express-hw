60c52c880eeebe446b6c61436d39f9c40280149f
Kaden Inskeep <tsukadenskeep@gmail.com>
2023-06-05 21:36:07 -0700

    routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 // GET Route for homepage
 router.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '../public/notes.html'))
+  res.sendFile(path.join(__dirname, '../notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
 router.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, '../index.html'))
 );
 
 module.exports = router;
